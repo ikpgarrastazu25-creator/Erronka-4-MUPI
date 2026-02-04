11b66aa715e8bd2ded13125f63c9cc8419e41c36
Urko <ikumendibe25@lhusurbil.eus>
2026-02-04 09:33:32 +0100

    karritoaren Zerbitzua eginda

---
+++ WEB_ORRIA/js/saskia.js
@@ -0,0 +1,10 @@
+const produktuKontainer = document.getElementById('produktuak');
+const kantitatea = document.getElementById('kantitatea');
+const prezioa = document.getElementById('prezioa');
+const hutsik = document.getElementById('karritoa_hutsik');
+const totalaKontainer = document.getElementById('totala');
+
+function sortuProduktuak() {
+    produktuKontainer.innerHTML = "";
+    const produktuak = JSON.parse(localStorage.getItem)
+}
